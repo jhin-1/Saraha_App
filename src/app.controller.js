2b8b0b817729ec480651145a8d12cc447671ce9d
jhin-1 <ahmedyosri52@gmail.com>
2026-03-10 02:59:59 +0200

    add redis connection

--- src/app.controller.js
+++ src/app.controller.js
@@ -5,7 +5,9 @@ import { globalErrorHandler } from './common/utils/response/index.js';
 import authRouter from './modules/auth/auth.controller.js';
 import messageRouter from './modules/messages/message.controller.js';
 import userRouter from './modules/user/user.controller.js';
+import { client, connectRedis } from './database/redis/connection.js';
 import cors from 'cors';
+
 
 export const bootstrap = async ()=>{
     const app = express();
@@ -14,22 +16,22 @@ export const bootstrap = async ()=>{
     app.use(express.json());
     app.use("/upload",express.static("upload"))
     app.use(cors())
+
     // routes
     app.use('/api/v1/auth',authRouter);
     app.use('/api/v1/message',messageRouter);
     app.use('/api/v1/users',userRouter);
+
     //database connection
     await DataBaseConnection();
-    
+    // connect to redis
+    await connectRedis();
+    // handel invalid route
     app.use('{*dummy}',(req,res)=>res.status(404).json("Invalid route"));
 
     // handel erorr 
     app.use(globalErrorHandler);
 
     
-
-
-
-
     app.listen(port,()=>console.log(`server is running on port ${port}`));
 }
