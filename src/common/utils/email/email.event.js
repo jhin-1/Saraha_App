d998cbe35b40ffebadfd721f4ba63a20bbb43c27
jhin-1 <ahmedyosri52@gmail.com>
2026-03-13 02:13:25 +0200

    add event email for using it in signup api for nottake match longer in responce

---
+++ src/common/utils/email/email.event.js
@@ -0,0 +1,23 @@
+import { EventEmitter } from "events";
+import { sendEmail } from "./sendemail.js";
+import { set } from "../../../database/redis/redis.service.js";
+
+let code = Math.random().toFixed(4).split(".")[1]
+
+let emailEvent = new EventEmitter();
+
+emailEvent.on("verifyEmail",async(data)=>{
+    let {email,userId} = data;
+    await set({
+            key:`Otp::${userId}`,
+            value:code,
+            ttl:60*5 // 5 minutes
+        })
+        await sendEmail({
+            to:email,
+            subject:"send verification email",
+            html:`<h1>verify your email with this code ${code}</h1>
+            <p>Please Enter the code to verify your email.</p>`
+        })
+})
+export default emailEvent;
