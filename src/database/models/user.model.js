cb97730bbf864b3f62f12541bc10b38adaa26a40
jhin-1 <ahmedyosri52@gmail.com>
2026-03-17 00:55:20 +0200

    update user model Password

--- src/database/models/user.model.js
+++ src/database/models/user.model.js
@@ -20,7 +20,9 @@ const UserSchema = new mongoose.Schema({
     },
     password:{
         type:String,
-        required:true
+        required:function(){
+            return this.provider === ProviderEnums.System
+        }
     },
     phone:String,
     DOB:Date,
